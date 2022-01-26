496315dce2fdf96804b335f442eaa98e7251045c
Sd <sdevone@outlook.fr>
2022-01-27 00:34:23 +0100

    modification of the modal

--- src/components/Requirements/Modal.jsx
+++ src/components/Requirements/Modal.jsx
@@ -1,17 +1,16 @@
-import React from "react"
+import React, {useState} from "react"
 import styled from "styled-components"
+import "./modal.css"
 
 const ModalComp = styled.div`
-  display: block;
+  width:100vw;
   height: 100vh;
-  overflow: hidden;
-  background-color: #00000055;
+  background-color: #00000050;
   background-color: ${(props) => props.bckg};
 `
-
 const ModalMsg = styled.div`
-  width: 20rem;
-  height: 15rem;
+  width: 25rem;
+  height: 10rem;
   border-radius: 1rem;
   background-color: #009879;
   background-color: ${(props) => props.txtBckg};
@@ -22,12 +21,13 @@ const ModalMsg = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 2.5rem;
+  font-size: 1.5rem;
+  color:#fff;
   color: ${(props) => props.color};
 `
 const CrossBtn = styled.button`
-  width: 2.5rem;
-  height: 2.5rem;
+  width: 1.6rem;
+  height: 1.6rem;
   position: absolute;
   top: 0;
   right: 0;
@@ -38,22 +38,22 @@ const CrossBtn = styled.button`
     cursor: pointer;
   }
 `
-
 export const Modal = ({
   message,
   background,
   txtBackground,
   txtColor,
 }) => {
-  const closeModal = () => {
-    document.getElementById("modalElement").style.display = "none"
-  }
+  const [isOpen,setIsOpen] = useState(false)
+  const showModal = "modalElement display-block"
+  const hideModal = "modalElement display-none"
+  const handleModal = isOpen ? hideModal : showModal
   return (
-    <div id="modalElement">
+    <div id={handleModal}>
       <ModalComp bckg={background}>
         <ModalMsg color={txtColor} txtBckg={txtBackground}>
           {message}
-          <CrossBtn onClick={closeModal}>x</CrossBtn>
+          <CrossBtn onClick={setIsOpen(true)}>x</CrossBtn>
         </ModalMsg>
       </ModalComp>
     </div>
